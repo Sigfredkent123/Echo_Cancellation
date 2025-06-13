53a6ed65a1ecad5d98c41b15b04ffc98291f685d
Sigfredkent123 <Sigfredkent123@gmail.com>
2025-06-13 13:54:37 +0800

    app.py

--- DSP_PROJECTFINAL/app.py
+++ DSP_PROJECTFINAL/app.py
@@ -121,4 +121,4 @@ def download_file(filename):
     return send_file(os.path.join(UPLOAD_FOLDER, filename), as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
